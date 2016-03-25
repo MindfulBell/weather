67306a89b7fd5a2a061f2febafa2ad04052b81bd
Tim Bell <tim.bell02@gmail.com>
2016-03-25 21:14:07 +0000

    setting up app

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
+import Searchbar from '../containers/search_bar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Searchbar />
+      </div>
     );
   }
 }
